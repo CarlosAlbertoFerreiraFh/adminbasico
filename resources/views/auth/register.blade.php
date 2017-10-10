a2e224c96221e70288d47169d9aaa7e4a5a9fbdf
Carlos Alberto <cf3100@gmail.com>
2017-10-10 14:46:04 -0300

    enviando

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Gentellela Alela! | </title>
+    
+    <!-- Bootstrap -->
+    <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{ asset("css/font-awesome.min.css") }}" rel="stylesheet">
+    <!-- Custom Theme Style -->
+    <link href="{{ asset("css/gentelella.min.css") }}" rel="stylesheet">
+
+</head>
+
+<body class="login">
+<div class="login_wrapper">
+    <div class="animate form login_form">
+        <section class="login_content">
+			{!! BootForm::open(['url' => url('/register'), 'method' => 'post']) !!}
+			
+			<h1>Cadastar Usuario</h1>
+
+			{!! BootForm::text('name', 'Name', old('name'), ['placeholder' => 'Full Name']) !!}
+
+			{!! BootForm::email('email', 'Email', old('email'), ['placeholder' => 'Email']) !!}
+
+			{!! BootForm::password('password', 'Password', ['placeholder' => 'Password']) !!}
+
+			{!! BootForm::password('password_confirmation', 'Password confirmation', ['placeholder' => 'Confirmation']) !!}
+		
+			{!! BootForm::submit('Cadastrar', ['class' => 'btn btn-primary col-md-offset-8 col-md-6']) !!}
+		   
+			<div class="clearfix"></div>
+			
+			<div class="separator">
+				<p class="change_link">Already a member ?
+					<a href="{{ url('/login') }}" class="to_register"> Login </a>
+				</p>
+				
+				<div class="clearfix"></div>
+				<br />
+				
+				<div>
+					<h1><i class="fa fa-paw"></i> AppsTecnologia</h1>
+					<p>©2017 Direitos reservados para Appstecnologia.</p>
+				</div>
+			</div>
+			{!! BootForm::close() !!}
+        </section>
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="pull-right">
+        AppsTecnologia - Desenvolvimento de Sistemas <a href="https://www.appstecnologia.com.br">AppsTecnologia</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
